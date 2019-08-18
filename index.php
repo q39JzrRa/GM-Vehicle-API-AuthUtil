96dceea33932025fd8a2e62ca9ffe11eddb95c76
Random Person <53839150+q39JzrRa@users.noreply.github.com>
2019-08-18 18:59:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 include 'JWT.php';
 use \Firebase\JWT\JWT;
 
-$key = "example_key";
+$brand = $_GET['brand'];
 
-$post = file_get_contents('php://input');
+$config = json_decode($_ENV['config'],true);
 
-$jwt = JWT::encode($post, $key);
+$brandConfig = $config[$brand];
+
+$payload = json_decode(file_get_contents('php://input'),true);
+
+
+
+$client_id = $brandConfig['client_id'];
+$client_secret = $brandConfig['client_secret'];
+
+$payload['client_id'] = $client_id;
+
+$jwt = JWT::encode($payload, $client_secret);
 
 Print $jwt;
 ?>
